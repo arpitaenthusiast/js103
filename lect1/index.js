64570168de58f5f2e7fea509939c110cc608f708
Arpita Das <ping.arpita@gmail.com>
2024-05-14 22:35:37 +0530

    feat: coded lecture 1 materials

---
+++ lect1/index.js
@@ -0,0 +1,26 @@
+console.log("Apnacollege");
+console.log("hii");
+console.log("js");
+
+//variable
+
+name = "tony";
+console.log(name);
+age = "21";
+console.log(age);
+
+isfollow = "true";
+console.log(isfollow);
+
+//let var 
+
+let fulname = "tommy";
+console.log(fulname);
+
+const fullname = "tom";
+console.log(fullname);
+
+{
+    let a = "5";
+    console.log(a);
+}
