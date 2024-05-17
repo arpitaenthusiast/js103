92f0f7b306b38d372c6c563d2c705ea1d68bdb5e
Arpita Das <ping.arpita@gmail.com>
2024-05-17 20:01:49 +0530

    unary operators & asignment operators

--- lect2/script.js
+++ lect2/script.js
@@ -7,3 +7,25 @@ let b = 1;
 console.log("a + b = ", a + b);
 console.log("a*b", a * b);
 console.log(a - b);
+
+
+//unary operator
+let c = 5;
+let d = 2;
+
+console.log("c = ", c, " & d = ", d);
+
+c++;
+console.log("c=", c);
+
+//Asignment Operators
+let e = 5;
+let f = 2;
+e += 4; //e= e+4
+console.log("e=", e);
+
+
+let A = 4;
+let B = 2;
+A -= 2;
+console.log("A =", A);
