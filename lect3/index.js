5f10014e9af5026a2bdc8d0ea342cd5eda9461cc
Arpita Das <ping.arpita@gmail.com>
2024-05-20 13:46:44 +0530

    loops (js)

---
+++ lect3/index.js
@@ -0,0 +1,14 @@
+//console.log("hello");
+//console.log("hello");
+//console.log("hello");
+//console.log("hello");
+//console.log("hello");
+
+
+//loop 
+//print 1 to 5 
+for (let count = 1; count <= 5; count++) {
+    console.log("hello world");
+}
+
+console.log("loop has ended");
