edbe6db14d7ac4f06eeaaeaee80ed65d8cd26949
Arpita Das <ping.arpita@gmail.com>
2024-05-21 12:33:28 +0530

    whilw loop and  for loop

--- lect3/index.js
+++ lect3/index.js
@@ -11,4 +11,16 @@ for (let count = 1; count <= 5; count++) {
     console.log("hello world");
 }
 
-console.log("loop has ended");
+//calculate sum 1 to 5
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i;
+// }
+// console.log("sum =", sum);
+// console.log("loop has ended");
+
+let i = 1
+while (i <= 5) {
+    console.log("my clg");
+    i++;
+}
