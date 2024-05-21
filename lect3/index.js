4c579b7eac48db5ac5920b8bdb2eae669928f12b
Arpita Das <ping.arpita@gmail.com>
2024-05-21 12:51:11 +0530

    do-while loop and  for-of loop

--- lect3/index.js
+++ lect3/index.js
@@ -19,8 +19,35 @@ for (let count = 1; count <= 5; count++) {
 // console.log("sum =", sum);
 // console.log("loop has ended");
 
-let i = 1
-while (i <= 5) {
-    console.log("my clg");
+// let i = 1
+// while (i <= 5) {
+//     console.log("my clg");
+//     i++;
+// }
+
+//Do while loop 
+
+let i = 1;
+// do {
+//     console.log("my college");
+//     i++;
+// }
+// while (i <= 20);
+do {
+    console.log("i=", i);
     i++;
 }
+while (i <= 5);
+
+//for-of loop
+// let str = "my home "
+// for (let i of str) {
+//     console.log("i=", i);
+// }
+let str = " javascript  "
+let size = 0;
+for (let i of str) {
+    console.log("i=", i);
+    size++;
+}
+console.log("string size", size);
