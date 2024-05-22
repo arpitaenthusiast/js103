dba2d4c36d39af69c6ef2c6d310b40a27540ec02
Arpita Das <ping.arpita@gmail.com>
2024-05-22 21:28:58 +0530

    array methods

--- lect4/index.js
+++ lect4/index.js
@@ -12,22 +12,32 @@
 //loops over  an array 
 
 //for loop 
-let color = ["red", "yellow", "green", "Blue"];
-for (let idx = 0; idx < color.length; idx++) {
-    console.log(color[idx]);
-}
+// let color = ["red", "yellow", "green", "Blue"];
+// for (let idx = 0; idx < color.length; idx++) {
+//     console.log(color[idx]);
+// }
 
-let cities = ["kolkata", " rampurhat", "nalhati", "delhi"];
-for (let city of cities) {
-    console.log(city.toUpperCase());
-}
+// let cities = ["kolkata", " rampurhat", "nalhati", "delhi"];
+// for (let city of cities) {
+//     console.log(city.toUpperCase());
+// }
 
 
 
-let marks = [85, 97, 44, 37, 76, 60];
-let sum = 0;
-for (let val of marks) {
-    sum += val;
-}
-let avg = sum / marks.length
-console.log(`avg marks of class = ${avg}`);
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// for (let val of marks) {
+//     sum += val;
+// }
+// let avg = sum / marks.length
+// console.log(`avg marks of class = ${avg}`);
+
+
+
+//array mathods
+//push
+let color = ["red", "green", "white", "blue"];
+//color.push("pink", "black", "golden");
+console.log(color);
+color.pop();
+console.log(color);
