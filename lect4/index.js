b0d5e61b74ea2e172ddecdab576ca7d637b088da
Arpita Das <ping.arpita@gmail.com>
2024-05-22 20:49:12 +0530

    array

---
+++ lect4/index.js
@@ -0,0 +1,9 @@
+//arrays
+//collections of items
+
+let marks = [97, 54, 33, 21, 11];
+console.log(marks);
+console.log(marks.length); //property
+
+let color = ["red", "yellow", "green", "Blue"];
+console.log(color);
