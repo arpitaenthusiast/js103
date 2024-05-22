60ce912e25e3fb20aad00280ae664e75174d0650
Arpita Das <ping.arpita@gmail.com>
2024-05-22 21:03:04 +0530

    loops of array(for loop)

--- lect4/index.js
+++ lect4/index.js
@@ -5,5 +5,19 @@ let marks = [97, 54, 33, 21, 11];
 console.log(marks);
 console.log(marks.length); //property
 
+// let color = ["red", "yellow", "green", "Blue"];
+// console.log(color);
+
+
+//loops over  an array 
+
+//for loop 
 let color = ["red", "yellow", "green", "Blue"];
-console.log(color);
+for (let idx = 0; idx < color.length; idx++) {
+    console.log(color[idx]);
+}
+
+let cities = ["kolkata", " rampurhat", "nalhati", "delhi"];
+for (let city of cities) {
+    console.log(city.toUpperCase());
+}
