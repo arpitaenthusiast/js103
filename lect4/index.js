4c1942383110e04486a15c1dfaf3b9bedc6ee200
Arpita Das <ping.arpita@gmail.com>
2024-05-22 21:14:50 +0530

    parctice

--- lect4/index.js
+++ lect4/index.js
@@ -1,9 +1,9 @@
 //arrays
 //collections of items
 
-let marks = [97, 54, 33, 21, 11];
-console.log(marks);
-console.log(marks.length); //property
+// let marks = [97, 54, 33, 21, 11];
+// console.log(marks);
+// console.log(marks.length); //property
 
 // let color = ["red", "yellow", "green", "Blue"];
 // console.log(color);
@@ -21,3 +21,13 @@ let cities = ["kolkata", " rampurhat", "nalhati", "delhi"];
 for (let city of cities) {
     console.log(city.toUpperCase());
 }
+
+
+
+let marks = [85, 97, 44, 37, 76, 60];
+let sum = 0;
+for (let val of marks) {
+    sum += val;
+}
+let avg = sum / marks.length
+console.log(`avg marks of class = ${avg}`);
