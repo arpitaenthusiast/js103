b23ea9b62f8c43bc9108f7f3db0b100fb6ffb6b7
Arpita Das <ping.arpita@gmail.com>
2024-05-23 20:37:34 +0530

    unctions in JavaScript can take input, perform tasks, and return values

--- lect5/script.js
+++ lect5/script.js
@@ -1,11 +1,28 @@
 // console.log("hello world ");
 
-// "abc".toUpperCase();
+// // "abc".toUpperCase();
 
 
-function myfunction() {
-    console.log("hello bharat")
-    console.log("We are indian")
+// function myfunction() {
+//     console.log("hello bharat")
+//     console.log("We are indian")
+// }
+
+// myfunction();
+
+
+function myfunction(msg) {
+    console.log(msg);
 }
+myfunction("jai hind");
 
-myfunction();
+
+//function --> 2 numbers , sum 
+function sum(x, y) {
+    s = x + y;
+    console.log("before return");
+    return s;
+    console.log("after return");
+}
+let val = sum(3, 4);
+console.log(val);
